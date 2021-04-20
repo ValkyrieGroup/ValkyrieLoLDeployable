98f5d0a251fd9b934d7c99e8dc851a5018cccd59
subzero-77 <82382783+subzero-77@users.noreply.github.com>
2021-04-20 18:02:22 +0200

    Update vk_olaf.py
    better Q for olaf (it now hits when you chase a target )

--- CoreScripts/vk_olaf.py
+++ CoreScripts/vk_olaf.py
@@ -3,12 +3,31 @@ from helpers.targeting import TargetSelector, TargetSet
 
 import helpers.templates as HT
 from   helpers.spells import SpellRotation, RSpell, Slot, SpellCondition, CCType
+
+class ConditionBehindTarget(SpellCondition):
+	def _check(self, ctx, player, target, spell):
+		return not player.in_front_of(target)
 		
+	def _get_name(self):
+		return 'Player in behind of target'
+	
+	def _get_help(self):
+		return 'Triggers when player is in behind of target'
+
+class OlafQPredictor:
+	def _predict(self, ctx, player, target, spell):
+		if spell.name == 'olafaxethrowcast':
+			return target.pos + (target.dir * 300)
+		else:
+			return ctx.predict_cast_point(player, target, spell)		
 olaf = HT.ChampionScript(
 	passive_trigger = HT.Enabler.default(),
 	combat_rotation = SpellRotation([
 		RSpell(Slot.E),
-		RSpell(Slot.Q), 
+		RSpell(Slot.Q,
+                       HT.ConditionInFrontOfTarget()),
+		RSpell(Slot.Q,
+                       ConditionBehindTarget(), predictor = OlafQPredictor()), 
 		RSpell(Slot.W,
 			HT.ConditionDistanceToTarget(0.0, 500.0)
 		),
